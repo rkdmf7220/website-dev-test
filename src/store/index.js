86904911b0ad5b42069da2390bf2740c7a4dd0e6
rkdmf7220 <rkdmf7220@gmail.com>
2023-01-29 21:39:08 +0900

    배포 전 파일로 변경

---
+++ src/store/index.js
@@ -0,0 +1,98 @@
+import {createStore} from 'vuex'
+import axios from "axios";
+import Constants from "../contants/constants";
+
+export default createStore({
+    state: {
+        lang: Constants.lang.en,
+        worksList: [],
+        cv: {},
+        contact: {},
+        review: []
+    },
+    mutations: {
+        lang(state, lang) {
+            state.lang = lang;
+        },
+        worksList(state, list) {
+            state.worksList = list;
+        },
+        cv(state, data) {
+            state.cv = data
+        },
+        contact(state, info) {
+            state.contact = info
+        },
+        review(state, data) {
+            state.review = data
+        }
+    },
+    actions: {
+        /**
+         *
+         * @param commit
+         * @param state
+         * @param dispatch
+         * @param lang
+         */
+        setLang({commit, state, dispatch}, lang) {
+            if(lang !== state.lang) {
+                commit('lang', lang);
+                dispatch('loadWorksList');
+                dispatch('loadCv');
+                dispatch('loadContact');
+                dispatch('loadReview');
+            }
+        },
+        loadWorksList({commit, state}) {
+            return axios.get("../sample/" + state.lang + "/data-result.json")
+                .then(response => {
+                    if (response.statusText === "OK") {
+                        commit('worksList', response.data)
+                    } else {
+                        // TODO: error handling.
+                    }
+                }).catch(error => {
+                console.log('error', error);
+                // TODO: error handling.
+            });
+        },
+        loadCv({commit, state}) {
+            axios.get("../sample/" + state.lang + "/cv.json")
+                .then(response => {
+                    if (response.statusText === "OK") {
+                        commit('cv', response.data)
+                    } else {
+                        // TODO: error handling.
+                    }
+                }).catch(error => {
+                console.log('error', error);
+            });
+        },
+        loadContact({commit, state}) {
+            axios.get("../sample/" + state.lang + "/contact.json")
+                .then(response => {
+                    if (response.statusText === "OK") {
+                        commit('contact', response.data)
+                    }   else {
+                        // TODO: error handling
+                    }
+                    }).catch(error => {
+                        console.log('error', error);
+            });
+        },
+        loadReview({commit, state}) {
+            axios.get("../sample/" + state.lang + "/review.json")
+                .then(response => {
+                    if (response.statusText === "OK") {
+                        commit('review', response.data)
+                    } else {
+                        //TODO: error handling
+                    }
+                }).catch(error => {
+                    console.log('error', error)
+            })
+        }
+    },
+    modules: {}
+})
